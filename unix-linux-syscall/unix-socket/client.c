7d990f81c0eaa7391e1bd2ca89a489224eebb2b9
fluidog <fluidog@163.com>
2022-05-31 22:22:52 +0800

    Add unix socket domain programming quize.

--- unix-linux-syscall/unix-socket/client.c
+++ unix-linux-syscall/unix-socket/client.c
@@ -7,56 +7,84 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-// #include <fcntl.h>
 
+#define SERVICE_SOCKET_ADDR "./socket"
+#define BUFFER_SIZE 1024
 
-void creat_socket()
+int creat_bind_socket(int type, char *socket_addr)
 {
 	int fd;
 	struct sockaddr_un addr;
 
-	fd = socket(AF_UNIX, SOCK_STREAM, 0);
+	fd = socket(AF_UNIX, type, 0);
 	if(fd < 0){
 		perror("socket");
-		exit(-1);
+		return -1;
 	}
 
 	addr.sun_family = AF_UNIX;
-	strcpy(addr.sun_path, "./socket");
-	unlink(addr.sun_path);
+	strcpy(addr.sun_path, socket_addr);
 
 	if(bind(fd, (struct sockaddr *)&addr, sizeof(addr)) < 0){
 		perror("bind");
-		exit(-1);
+		return -1;
 	}
-
-	if(listen(fd, 5) < 0){
-		perror("listen");
-		exit(-1);
-	}
-
-	while(1){
-		int conn_fd;
-		struct sockaddr_un conn_addr;
-		socklen_t conn_len;
-
-		conn_fd = accept(fd, (struct sockaddr *)&conn_addr, &conn_len);
-		if(conn_fd < 0){
-			perror("accept");
-			exit(-1);
-		}
-
-		printf("accepted\n");
-		close(conn_fd);
-	}
-
-
+	printf("bind socket addr:%s\n", addr.sun_path);
+	return fd;
 }
 
+void unix_dgram()
+{
+	int fd;
+	struct sockaddr_un addr; // service addr
+	size_t size;
+	char buf[BUFFER_SIZE];
+
+	fd  = creat_bind_socket(SOCK_DGRAM, tmpnam(NULL)); // tmp local addr
+	if(fd < 0)
+		exit(-1);
+	
+	addr.sun_family = AF_UNIX;
+	strcpy(addr.sun_path, SERVICE_SOCKET_ADDR);
+	
+	size = sendto(fd, "hello", 6, 0, (const struct sockaddr *)&addr, sizeof(addr));
+	printf("sendto size: %ld\n", size);
+
+	size = recvfrom(fd, buf, BUFFER_SIZE, 0, NULL, NULL);
+	printf("recvfrom size:%ld: %s\n", size, buf);
+
+	// close(fd);  // close automatic when exit
+}
+
+void unix_stream()
+{
+	int fd;
+	char buf[BUFFER_SIZE];
+	size_t size;
+	struct sockaddr_un addr; // service addr
+	socklen_t addr_len = sizeof(addr);
+
+	fd  = creat_bind_socket(SOCK_STREAM, tmpnam(NULL)); // tmp local addr
+	if(fd < 0)
+		exit(-1);
+	
+	addr.sun_family = AF_UNIX;
+	strcpy(addr.sun_path, SERVICE_SOCKET_ADDR);
+
+	connect(fd, (struct sockaddr *)&addr, sizeof(addr));
+
+	size = write(fd, "hello", 6);
+	printf("write size: %ld\n", size);
+
+	size = read(fd, buf, BUFFER_SIZE);
+	printf("read size: %ld: %s\n", size, buf);
+}
 
 int main()
 {
-	creat_socket();
+
+	// unix_dgram();
+	unix_stream();
 
 	return 0;
 }
--- unix-linux-syscall/unix-socket/service.c
+++ unix-linux-syscall/unix-socket/service.c
@@ -2,34 +2,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <error.h>
+#include <string.h>
 
 #include <sys/un.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-// #include <fcntl.h>
 
+#define SOCKET_ADDR "./socket" // Relative path is a not good idea, but for convenience.
+#define BUFFER_SIZE 1024
 
-void creat_socket()
+int creat_bind_socket(int type, char *socket_addr)
 {
 	int fd;
 	struct sockaddr_un addr;
 
-	fd = socket(AF_UNIX, SOCK_STREAM, 0);
+	fd = socket(AF_UNIX, type, 0);
 	if(fd < 0){
 		perror("socket");
-		exit(-1);
+		return -1;
 	}
 
 	addr.sun_family = AF_UNIX;
-	strcpy(addr.sun_path, "./socket");
+	strcpy(addr.sun_path, socket_addr);
 	unlink(addr.sun_path);
 
 	if(bind(fd, (struct sockaddr *)&addr, sizeof(addr)) < 0){
 		perror("bind");
-		exit(-1);
+		return -1;
 	}
+	return fd;
+}
 
+void unix_dgram()
+{
+	int fd;
+	char buf[BUFFER_SIZE];
+	size_t size;
+	struct sockaddr_un addr; // client addr
+	socklen_t addr_len = sizeof(addr);
+
+	fd = creat_bind_socket(SOCK_DGRAM, SOCKET_ADDR);
+	if(fd < 0)
+		exit(-1);
+
+	// not exit
+	while (1)
+	{
+		size = recvfrom(fd, buf, BUFFER_SIZE, 0, (struct  sockaddr *)&addr, &addr_len);
+		if(size < 0){
+			perror("recvfrom");
+			exit(-1);
+		}
+		printf("recvfrom size: %ld: %s\n", size, buf);
+
+		// client addr is not set.
+		if(addr_len == 0)
+			continue;
+
+		size = sendto(fd,buf, size, 0, (const struct sockaddr *)&addr, sizeof(addr));
+		printf("sendto: %s size: %ld\n", addr.sun_path, size);
+	}
+}
+
+
+
+void unix_stream()
+{
+	int fd;
+	char buf[BUFFER_SIZE];
+	size_t size;
+	
+
+	fd = creat_bind_socket(SOCK_STREAM, SOCKET_ADDR);
+	if(fd < 0)
+		exit(-1);
+
+	
 	if(listen(fd, 5) < 0){
 		perror("listen");
 		exit(-1);
@@ -37,26 +87,55 @@ void creat_socket()
 
 	while(1){
 		int conn_fd;
-		struct sockaddr_un conn_addr;
-		socklen_t conn_len;
+		struct sockaddr_un addr; // client addr
+		socklen_t addr_len;
 
-		conn_fd = accept(fd, (struct sockaddr *)&conn_addr, &conn_len);
+		addr_len = sizeof(addr);
+		conn_fd = accept(fd, (struct sockaddr *)&addr, &addr_len);
 		if(conn_fd < 0){
 			perror("accept");
 			exit(-1);
 		}
+		printf("accepted from :%s\n", addr.sun_path);
 
-		printf("accepted\n");
-		close(conn_fd);
+		pid_t pid = fork();
+		if(pid < 0){
+			perror("fork");
+			exit(-1);
+		}
+
+		// parent process continue accept new connection. 
+		if(pid > 0){
+			close(conn_fd); // conn_fd is not used in parent process.
+			// wait(); There have a small issue - child process will become zombies when they die.
+			continue;
+		}
+
+		// child process read and write data.
+		close(fd); // close listen fd.
+		while(1){
+			size = read(conn_fd, buf, sizeof(buf));
+			if(size == 0) // client close connection.
+				exit(0);
+			if(size < 0){
+				perror("read");
+				exit(-1);
+			}
+			printf("read size: %ld: %s\n", size, buf);
+			size = write(conn_fd, buf, size);
+			printf("write size: %ld\n", size);
+		}
+		
+		// close(conn_fd); // close automatic when exit.
+		exit(0);
 	}
-
-
 }
-
 
 int main()
 {
-	creat_socket();
+
+	// unix_dgram();
+	unix_stream();
 
 	return 0;
 }
